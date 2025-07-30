c6d38f8775c42518785731fe376bcc6140ca8e86
ac-gh-19 <chanandrew1925@gmail.com>
2025-07-30 05:12:13 -0700

    implement equal button and shuntingyardalgorithm

--- javascript.js
+++ javascript.js
@@ -5,12 +5,19 @@ let clearBtn = document.querySelector(".clearBtn");
 let delBtn = document.querySelector(".delBtn");
 let dotBtn = document.querySelector(".dotBtn");
 let oppSignBtn = document.querySelector(".oppositeSignBtn");
+let modBtn = document.querySelector(".modBtn");
+let equalBtn = document.querySelector(".equalBtn");
+let outputTxt = document.querySelector(".outputText");
+
+let allBtns = document.querySelectorAll(".buttons");
 
 
 let equation = "";
 let lastInput = "";
 let lastOperand = "";
-let operators = "+-/x"
+let operators = "+-/x";
+let equalSignLastClicked = false;
+
 
 function getLastInput() { return lastInput; };
 function isOperator(value) { return operators.includes(value); };
@@ -23,10 +30,25 @@ function updateLastOperand(currInput) {
         lastOperand += currInput;
     }
 }
+function isHigherPrecendence(topOp, newOp) {
+    let prios = {
+        "+": 1,
+        "-": 1,
+        "x": 2,
+        "/": 2,
+    }
+
+    if (prios[topOp] >= prios[newOp]) return true;
+    return false;
+}
 
 
 numBtns.forEach(btn => {
     btn.addEventListener("click", e => {
+        if (equalSignLastClicked) {
+            clearBtn.click();
+            equalSignLastClicked = false;
+        }
         inputText.value += e.target.textContent;
         equation += e.target.textContent;
         updateLastOperand(e.target.textContent);
@@ -38,6 +60,9 @@ numBtns.forEach(btn => {
 
 expBtns.forEach(btn => {
     btn.addEventListener("click", (e) => {
+        if (equalSignLastClicked) {
+            equalSignLastClicked = false;
+        }
         if (isOperator(getLastInput())) { // if clicking expressionBtn twice - remove the last one
             inputText.value = inputText.value.slice(0, -1);
             equation = inputText.value.slice(0,-1);
@@ -66,6 +91,7 @@ clearBtn.addEventListener("click", (e) => {
     lastInput = "";
     equation = "";
     lastOperand = "";
+    outputTxt.textContent = "";
 })
 
 delBtn.addEventListener("click", (e) => {
@@ -136,4 +162,93 @@ dotBtn.addEventListener("click", (e) => {
     updateLastInput();
     if (lastInput != "." ) inputText.value += e.target.textContent;
     inputText.scrollLeft = inputText.scrollWidth;
+})
+
+modBtn.addEventListener("click", (e) => {
+    alert("will implement later");
+    clearBtn.click();
+})
+
+equalBtn.addEventListener("click", (e) => {
+    if (equation == "") return;
+    if (equation[0] == "-") equation = "0" + equation;
+    if (equation.includes("/0")) {
+        inputText.value = "Invalid";
+        setTimeout(() => {
+            clearBtn.click();
+        }, 1000);
+        return;
+    }
+    if (equation.at(-1) == ".") equation += "0";
+    if (operators.includes(equation.at(-1))) equation = equation.slice(0, -1);
+
+
+    let rpnQueue = [];
+    let operatorStack = [];
+
+    let value = "";
+    for (let i = 0; i < equation.length; ++i) {
+        if (!operators.includes(equation[i])) {
+            value += equation[i];
+        } else if (operators.includes(equation[i])) {
+            rpnQueue.push(+value);
+            if (operatorStack.length == 0) {
+                operatorStack.push(equation[i]);
+            } else {
+                let isTopOpHigher = isHigherPrecendence(operatorStack.at(-1), equation[i]);
+                if (isTopOpHigher) {
+                    rpnQueue.push(operatorStack.pop());
+                    operatorStack.push(equation[i]);
+                } else {
+                    operatorStack.push(equation[i]);
+                }
+            }
+            value = "";
+        }
+    }
+
+    if (value !== "") {
+        rpnQueue.push(+value);
+    }
+
+    while (operatorStack.length != 0) {
+        rpnQueue.push(operatorStack.pop());
+    }
+
+    console.log(rpnQueue);
+
+    let total = 0;
+    let stack = [];
+    for (let i = 0; i < rpnQueue.length; ++i) {
+        if (typeof rpnQueue.at(i) == "number") {
+            stack.push(rpnQueue.at(i));
+        } else {
+            let num1 = stack.pop();
+            let num2 = stack.pop();
+            let result = 0;
+            switch (rpnQueue.at(i)) {
+                case "x": 
+                    result = num2 * num1;
+                    break;
+                case "/":
+                    result = num2 / num1;
+                    break;
+                case "+":
+                    result = num2 + num1;
+                    break;
+                case "-":
+                    result = num2 - num1;
+                    break;
+            }
+        stack.push(result);
+        }
+    }
+
+    inputText.value = stack[0];
+    outputTxt.textContent = equation + " = " + stack[0];
+    outputTxt.scrollLeft = outputTxt.scrollWidth;
+    equation = inputText.value;
+    equalSignLastClicked = true;
+
+
 })
